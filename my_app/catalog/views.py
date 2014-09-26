8c3656cbe261c362d5698ad3bf510882b20abd90
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-09-26 13:43:12 +0530

    Chapter 4 - Recipe 'Decorator for handling requests beautifully'

--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -1,18 +1,35 @@
+from functools import wraps
 from flask import request, Blueprint, render_template, jsonify
 from my_app import db
 from my_app.catalog.models import Product, Category
 
 catalog = Blueprint('catalog', __name__)
 
+
+def template_or_json(template=None):
+    """"Return a dict from your view and this will either
+    pass it to a template or render json. Use like:
+
+    @template_or_json('template.html')
+    """
+    def decorated(f):
+        @wraps(f)
+        def decorated_fn(*args, **kwargs):
+            ctx = f(*args, **kwargs)
+            if request.is_xhr or not template:
+                return jsonify(ctx)
+            else:
+                return render_template(template, **ctx)
+        return decorated_fn
+    return decorated
+
+
 @catalog.route('/')
 @catalog.route('/home')
+@template_or_json('home.html')
 def home():
-    if request.is_xhr:
-        products = Product.query.all()
-        return jsonify({
-            'count': len(products)
-        })
-    return render_template('home.html')
+    products = Product.query.all()
+    return {'count': len(products)}
 
 
 @catalog.route('/product/<id>')
